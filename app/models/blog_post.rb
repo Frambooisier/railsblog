dc2ad21f8ab6af4b4ce50fb0b74ff298bcf72e0f
Burden_Lifebook <frambooisier@gmail.com>
2025-09-23 09:20:52 +0200

    binnaries changed from bundler to something else + added database enviroment variable tag

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,3 +3,11 @@
           validates :title, presence: true
           validates :body, presence: true
 end
+
+        # status string field
+        # - draft
+        # - published
+        # 'published_at' datetime field
+        # - nil
+        # - 1.year.ago
+        # - 1.year.from_now
